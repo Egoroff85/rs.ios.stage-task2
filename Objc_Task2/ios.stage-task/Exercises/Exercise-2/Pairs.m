59352b057a87164c2a8ba4e3c3f6d6c75f88e611
Andrey Egorov <andreyegorov1985@gmail.com>
2021-05-15 17:03:37 +0300

    feat: add exercises 1,2,3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger counter = 0;
+    for (int i = 0; i < array.count - 1; i++) {
+        for (int j = i + 1; j < array.count; j++) {
+            if (fabs([[array objectAtIndex:i] doubleValue] - [[array objectAtIndex:j] doubleValue]) == [number doubleValue]) {
+                counter += 1;
+            }
+        }
+    }
+    return counter;
 }
 
 @end
