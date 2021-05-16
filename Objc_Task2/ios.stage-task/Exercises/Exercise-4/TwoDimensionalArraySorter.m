d2ad3ea83e51c1c8f443e9baca30fa8ceffeed67
Andrey Egorov <andreyegorov1985@gmail.com>
2021-05-16 03:55:07 +0300

    feat: add exercise 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,32 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (nil == array) {
+        return @[];
+    }
+    
+    NSPredicate *arrPred = [NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [NSArray class]];
+    NSArray *filteredArrays = [array filteredArrayUsingPredicate:arrPred];
+    if (filteredArrays.count == 0) {
+        return @[];
+    }
+    
+    NSArray *flatArray = [array valueForKeyPath:@"@unionOfArrays.self"];
+
+    NSPredicate *numberPred = [NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [NSNumber class]];
+    NSPredicate *stringPred = [NSPredicate predicateWithFormat:@"self isKindOfClass: %@", [NSString class]];
+    NSArray *filteredNumbers = [flatArray filteredArrayUsingPredicate:numberPred];
+
+    if (filteredNumbers.count == flatArray.count || filteredNumbers.count == 0) {
+        return [flatArray sortedArrayUsingSelector: @selector(compare:)];
+    }
+    else {
+        NSArray *numbers = [filteredNumbers sortedArrayUsingSelector: @selector(compare:)];
+        NSArray *filteredStrings = [flatArray filteredArrayUsingPredicate:stringPred];
+        NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO selector:@selector(compare:)];
+        NSArray *strings = [filteredStrings sortedArrayUsingDescriptors:@[sortDescriptor]];
+        return @[numbers, strings];
+    }   
 }
 
 @end
